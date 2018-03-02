b401e99b502f3ac4c2c7b8020286f03df7158a48
FarkIst <FarkIst@github.com>
2018-03-02 12:37:24 +0100

    initial setup

---
+++ Assets/CircleSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpawner : MonoBehaviour {
+
+    public GameObject circle;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Instantiate(circle, mousePos, Quaternion.identity);
+        }
+	}
+
+
+}
